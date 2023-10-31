7cc05194c2df8284c71aaad997c247cb80c77d94
getEldarK <elderyendex@yandex.ru>
2023-10-31 18:48:32 +0500

    пр13 создание схем  user and card js

---
+++ models/card.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      validate: {
+        validator: ({ length }) => length >= 2 && length <= 30,
+        message: 'Имя карточки должно быть длиной от 2 до 30 символов',
+      },
+    },
+
+    link: {
+      type: String,
+      required: true,
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+
+    likes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    }],
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+module.exports = mongoose.model('card', cardSchema);
