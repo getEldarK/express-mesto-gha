4e4329e0f6d676a9dec6d7645462dab21a0686e1
getEldarK <elderyendex@yandex.ru>
2023-11-01 12:23:01 +0500

    пр13 создал контролеры и роуты для юзера

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users'); // импортируем роутер users.js
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true
 });
+
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6541faeca8f3af78ff187071' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
+app.use('/', users);
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
