ca5642fd442834d9f9a34081f3b4a92196e0435d
getEldarK <elderyendex@yandex.ru>
2023-11-22 22:21:02 +0500

    fixed error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = require('../utils/config');
+
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
@@ -11,15 +12,17 @@ module.exports = (req, res, next) => {
 
   // убеждаемся, что он есть
   if (!token) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('С токеном что-то не так'));
   }
+
+  let payload;
 
   try {
     // попытаемся верифицировать токен
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     // отправим ошибку, если не получилось
-    return next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('С токеном что-то не так'));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
 
